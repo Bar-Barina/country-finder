e5b832d5e2673926ac8f925070307c7bb3948b80
Bar Barina <itsbarbarina@gmail.com>
2024-01-07 12:41:36 +0200

    Fixed noResults modal sass

--- src/app/components/NoResultsModal.jsx
+++ src/app/components/NoResultsModal.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 
 function NoResultsModal() {
   return (
-    <section className='no-results-container'>
-      <img src='../../NoResults.jpg' alt='No Results Found Logo' />
-
+    <section className='no-results-container flex column auto-center'>
+      <img src='../../NoResults.png' alt='No Results Found Logo' />
       <div>No Results Found.</div>
     </section>
   )
