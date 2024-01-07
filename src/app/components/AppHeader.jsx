904e9c961a7b84548c6e703b84ae5bb4e023cc25
Bar Barina <itsbarbarina@gmail.com>
2024-01-07 09:56:22 +0200

    Deleted unused h5 from app header

--- src/app/components/AppHeader.jsx
+++ src/app/components/AppHeader.jsx
@@ -4,7 +4,6 @@ function AppHeader() {
   return (
     <section className='app-header-container align-center'>
       <h1>Country 🌍 Finder</h1>
-      {/* <h5>The BEST, FREE OF CHARGE Search Engine Out There.</h5> */}
     </section>
   )
 }
