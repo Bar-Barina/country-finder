5ccc338722ea0703c04136feb213efe20a0798d3
Bar Barina <itsbarbarina@gmail.com>
2024-01-07 23:40:57 +0200

    Moved no results logic to LiveSearch cmp, fixed layout for mobile.

--- src/app/components/CountryList.jsx
+++ src/app/components/CountryList.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
 import CountryPreview from './CountryPreview'
-import NoResultsModal from './NoResultsModal'
 
 function CountryList({ countries }) {
   return (
     <section className='results-container'>
-      {/* Checking if there are no countries, if so, no results modal will render, otherwise render the results*/}
-      {countries.length === 0 ? (
-        <NoResultsModal />
-      ) : (
-        countries.map((country) => (
-          <CountryPreview key={country.alpha3Code} country={country} />
-        ))
-      )}
+      {countries.map((country) => (
+        <CountryPreview key={country.alpha3Code} country={country} />
+      ))}
     </section>
   )
 }
--- src/app/components/LiveSearch.jsx
+++ src/app/components/LiveSearch.jsx
@@ -4,33 +4,43 @@ import { apiService } from '../services/api.service'
 import { utilService } from '../services/util.service'
 import CountryList from './CountryList'
 import Loader from './Loader'
+import RegionSort from './RegionSort'
+import NoResultsModal from './NoResultsModal'
 
 function LiveSearch() {
-  // Using and modifying state here, because i don't need to use this data any other place.
   const [query, setQuery] = useState('')
   const [results, setResults] = useState([])
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
+  const [selectedRegion, setSelectedRegion] = useState('')
 
-  // Using the debounce function from my util service
   const debouncedFetchData = utilService.debounce(fetchData, 300)
 
   useEffect(() => {
-    // Calling the debouncedFetchData function on query change
     debouncedFetchData()
-  }, [query])
+  }, [query, selectedRegion])
 
   async function fetchData() {
     try {
       setError(null)
       setLoading(true)
       const countriesList = await apiService.getCountriesList()
+      // Init filteredResults out scope to modify it based on the query/selectedRegion
+      let filteredResults = countriesList
+
       if (query) {
-        setResults(countriesList)
-        console.log(countriesList)
+        filteredResults = countriesList.filter((country) =>
+          country.name.toLowerCase().startsWith(query.toLowerCase())
+        )
       }
+      if (selectedRegion) {
+        filteredResults = filteredResults.filter(
+          (country) => country.region === selectedRegion
+        )
+      }
+      setResults(filteredResults)
     } catch (error) {
-      setError('Error fetching data')
+      setError('Error fetching data', error)
     } finally {
       setLoading(false)
     }
@@ -45,29 +55,37 @@ function LiveSearch() {
     }
   }
 
+  const handleRegionSort = (region) => {
+    setSelectedRegion(region)
+  }
+
   return (
     <section className='live-search-container flex column'>
-      <input
-        className='flex auto-center'
-        type='text'
-        placeholder='Search for a country'
-        value={query}
-        onChange={handleInputChange}
-      />
-      {/* Checking if there is a query, if there is, let the user sort by region
-{query && <RegionSort onSort={handleRegionSort} />} */}
+      <div className='search-wrapper'>
+        <input
+          className='flex auto-center'
+          type='text'
+          placeholder='Search for a country'
+          value={query}
+          onChange={handleInputChange}
+        />
+        {/* Checking if there is a query, if there is, let the user sort by region */}
+        {query && <RegionSort onSort={handleRegionSort} />}
+
+        {!query && <span>Supporting ISRAEL 💙</span>}
+      </div>
+
       {loading && <Loader />}
 
       {/* If there is an error fetching the data, inform the user */}
       {error && <div className='error-message'>{error}</div>}
 
-      {/* Checking if there is results and query, then sending the already filtered results to the CountryList component */}
+      {/* Render NoResultsModal only when loading is false */}
+      {!loading && results.length === 0 && query && <NoResultsModal />}
+
+      {/* Render the results when there are results and loading is false */}
       {!loading && results.length > 0 && query && (
-        <CountryList
-          countries={results.filter((country) =>
-            country.name.toLowerCase().startsWith(query.toLowerCase())
-          )}
-        />
+        <CountryList countries={results} />
       )}
     </section>
   )
